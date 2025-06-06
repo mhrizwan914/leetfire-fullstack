6bb1c498c59f250c17660521e9770215892a151c
mhrizwan914 <mhrizwan914@gmail.com>
2025-06-07 01:47:42 +0500

    fix frontend base api url

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,8 +1,10 @@
 // Axios
 import axios from "axios";
 
+const baseURL = import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1" : "/api/v1";
+
 export const axios_instance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL,
   withCredentials: true,
   // timeout: 5000,
   headers: {
